e4f4ac6fbc245f0f73b1ddc0612d93f4fdd17032
emoticonyt <doriandelvalle67@gmail.com>
2026-03-22 18:32:17 -0500

    Create gui.sh

---
+++ gui.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Starting Realistic Battery"
+
+read -p "Please install Python3 before running this script. If you get any cannot find "python3" or module not found "pip" errors, you may need to install Python or PipPress enter to continue..." pressentertocontinuevariablenotimportantdontcaredidntasklollmaostfuidc
+
+prevdir="$PWD"
+
+cd /tmp
+
+rm -rf realisticbattery
+
+mkdir -p /tmp/realisticbattery
+
+cd realisticbattery
+
+curl -LO "https://raw.githubusercontent.com/EmoticonYT/realistic_battery/refs/heads/main/realisticbattery/realistic_battery.py"
+
+curl -LO "https://raw.githubusercontent.com/EmoticonYT/realistic_battery/refs/heads/main/realisticbattery/Realistic%20Battery.spec"
+
+curl -LO "https://raw.githubusercontent.com/EmoticonYT/realistic_battery/refs/heads/main/realisticbattery/requirements.txt"
+
+python3 -m pip install -r requirements.txt
+
+python3 realistic_battery.py --gui
+
+echo "Running Now!"
