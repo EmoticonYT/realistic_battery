b6c66a94032736b4088a807e44ded638d83ce149
emoticonyt <doriandelvalle67@gmail.com>
2026-03-23 06:48:23 -0500

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -2,7 +2,7 @@
 
 echo "Starting Realistic Battery"
 
-read -p "Please install Python3 before running this script. If you get any cannot find "python3" or module not found "pip" errors, you may need to install Python or PipPress enter to continue..." pressentertocontinuevariablenotimportantdontcaredidntasklollmaostfuidc
+read -p "Please install Python3 before running this script. If you get any cannot find "python3" or module not found "pip" errors, you may need to install Python or Pip Press enter to continue..." pressentertocontinuevariablenotimportantdontcaredidntasklollmaostfuidc
 
 prevdir="$PWD"
 
@@ -24,6 +24,8 @@ python3 -m pip install -r requirements.txt --break-system-packages
 
 python3 realistic_battery.py --gui
 
+PYPID=$!
+
 echo "Running Now!"
 
-trap 'cd "$prevdir"; echo "Thanks for using Realistic Battery!"; exit' INT
+wait $PYPID; echo "Thanks for using Realistic Battery!"; exit' INT
